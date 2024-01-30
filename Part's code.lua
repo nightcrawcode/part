fd7c151deb097ab8e9263fff010450096734e57e
Martin <143912975+nightcrawcode@users.noreply.github.com>
2024-01-29 20:22:05 -0800

    Create Part's code.lua

---
+++ Part's code.lua
@@ -0,0 +1,22 @@
+
+-- Warning: This script was automatically generated using the EventBlocks plugin.
+
+-- Any modification to this script using Roblox’s default scripting editor may result in a broken script in the EventBlocks editor.
+-- Therefore, to edit this please use EventBlocks (find on devforum)
+
+
+
+
+
+-- Do not modify anything below here! --
+
+coroutine.wrap(function()
+--START--
+--[[Idk why I made this. I was board--]]--[[Welp your welcome--]]while true do
+game:GetService("Chat"):Chat(script.Parent, tostring((tostring([[Lol]]))))
+
+task.wait()
+end
+
+-------
+end)()
